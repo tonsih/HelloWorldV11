2afa74f1d4bf4999de2b689a8265d99a76543de8
karruc-9 <karruc.9@student.ltu.se>
2022-04-05 18:03:06 +0200

    more great comments, this time without your consent :)

--- src/main/java/HelloWorld11.java
+++ src/main/java/HelloWorld11.java
@@ -3,6 +3,7 @@ public class HelloWorld11 {
     public static void main(String[] args) {
         System.out.println("Hello world 11!");
         // Hello my amazing Study Buddy!!!
+        // Now I control your repository as well! Don't need your permission!
     }
 
 }
